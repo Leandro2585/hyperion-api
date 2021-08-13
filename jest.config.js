c4847c9218f3ef5e588d3a818ef6d2f6ba0f6df1
Leandro2585 <leo.real2585@gmail.com>
2021-08-12 22:05:47 -0300

    chore: merge branch chore/setup_project

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    '@tests/(.+)': '<rootDir>/tests/$1',
+    '@main/(.+)': '<rootDir>/src/main/$1',
+    '@domain/(.+)': '<rootDir>/src/domain/$1',
+    '@data/(.+)': '<rootDir>/src/data/$1',
+    '@infra/(.+)': '<rootDir>/src/infra/$1',
+    '@application/(.+)': '<rootDir>/src/application/$1',
+    '@validation/(.+)': '<rootDir>/src/validation/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts': 'ts-jest'
+  }
+}
