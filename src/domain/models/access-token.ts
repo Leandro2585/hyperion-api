a1708869c823c1a14dd58b5e5244056cb2534742
Leandro2585 <leo.real2585@gmail.com>
2021-08-18 21:37:50 -0300

    feat: ensure AccessToken expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,5 +1,7 @@
 export class AccessToken {
-  constructor (private readonly value: string) {
+  constructor (private readonly value: string) {}
 
+  static get expirationInMs (): number {
+    return 30 * 60 * 1000
   }
 }
