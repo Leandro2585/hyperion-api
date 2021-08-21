c3870e0e8ff5512395e2d4573cfe012debc42827
Leandro2585 <leo.real2585@gmail.com>
2021-08-21 18:14:46 -0300

    feat: ensure facebook api return facebook user

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,7 +1,7 @@
 import { LoadFacebookUserApi } from '@data/protocols/gateways'
 import { HttpGetClient } from '@infra/http'
 
-export class FacebookApi {
+export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
   constructor (
     private readonly httpClient: HttpGetClient,
@@ -10,7 +10,7 @@ export class FacebookApi {
 
   ) {}
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
@@ -29,7 +29,7 @@ export class FacebookApi {
       }
     })
 
-    await this.httpClient.get({
+    const userInfo = await this.httpClient.get({
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
@@ -37,5 +37,11 @@ export class FacebookApi {
         access_token: params.token
       }
     })
+
+    return {
+      name: userInfo.name,
+      email: userInfo.email,
+      facebookId: userInfo.id
+    }
   }
 }
