630dd1b93b0166d28f12b4d3b10330c64e060a4c
Leandro2585 <leo.real2585@gmail.com>
2021-08-21 17:46:30 -0300

    test: ensure facebook api gets user info

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,7 +15,9 @@ describe('facebook api', () => {
     httpClient = mock()
   })
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({ access_token: 'any_app_token' })
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' })
+      .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -40,6 +42,18 @@ describe('facebook api', () => {
       params: {
         access_token: 'any_app_token',
         input_token: 'any_client_token'
+      }
+    })
+  })
+
+  test('should get user info', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/any_user_id',
+      params: {
+        fields: 'id,name,email',
+        access_token: 'any_client_token'
       }
     })
   })
