e7b98a184965df632a49f25b808f63f97d2aaaf8
Leandro2585 <leo.real2585@gmail.com>
2021-08-24 19:41:00 -0300

    feat: make JwtTokenGenerator Adapter implement TokenGenerator

--- src/infra/cryptography/jwt-token-generator-adapter.ts
+++ src/infra/cryptography/jwt-token-generator-adapter.ts
@@ -2,7 +2,7 @@ import { sign } from 'jsonwebtoken'
 
 import { TokenGenerator } from '@data/protocols/cryptography'
 
-export class JwtTokenGeneratorAdapter {
+export class JwtTokenGeneratorAdapter implements TokenGenerator {
   constructor (private readonly secret: string) {}
   async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000
