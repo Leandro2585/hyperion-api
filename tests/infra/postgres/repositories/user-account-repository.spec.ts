5ecd361c9910f7dc609c7a555851c5b8f5a38229
Leandro2585 <leo.real2585@gmail.com>
2021-08-24 21:42:54 -0300

    test: ensure PostgresUserAccountRepository loads account if emaio exists

--- tests/infra/postgres/repositories/user-account-repository.spec.ts
+++ tests/infra/postgres/repositories/user-account-repository.spec.ts
@@ -1,17 +1,51 @@
+import { newDb } from 'pg-mem'
+
 import { ILoadUserAccountRepository } from '@data/protocols/repositories'
+import { Entity, PrimaryGeneratedColumn, Column, getRepository } from 'typeorm'
 
 class PostgresUserAccountRepository implements ILoadUserAccountRepository {
   async load (params: ILoadUserAccountRepository.Params): Promise<ILoadUserAccountRepository.Result> {
-    return await Promise.resolve(undefined)
+    const postgresUserRepository = getRepository(PostgresUser)
+    const postgresUser = await postgresUserRepository.findOne({ email: params.email })
+    if (postgresUser !== undefined) {
+      return {
+        id: postgresUser.id.toString(),
+        name: postgresUser.name ?? undefined
+      }
+    }
   }
+}
+
+@Entity({ name: 'users' })
+class PostgresUser {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({ name: 'name', nullable: true })
+  name?: string
+
+  @Column()
+  email!: string
+
+  @Column({ name: 'id_facebook', nullable: true })
+  facebookId?: string
 }
 
 describe('user-account repository', () => {
   describe('load', () => {
     test('', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PostgresUser]
+      })
+      await connection.synchronize()
+      const postgresUserRepository = getRepository(PostgresUser)
+      await postgresUserRepository.save({ email: 'existing_email' })
       const sut = new PostgresUserAccountRepository()
+      const account = await sut.load({ email: 'existing_email' })
 
-      await sut.load({ email: 'existing_email' })
+      expect(account).toEqual({ id: '1' })
     })
   })
 })
