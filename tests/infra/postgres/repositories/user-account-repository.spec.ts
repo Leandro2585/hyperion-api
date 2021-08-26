c5af624c932c15b95ec05f3592b5c77b6e3f2dda
Leandro2585 <leo.real2585@gmail.com>
2021-08-25 21:25:31 -0300

    test: ensure PostgresUserAccountRepository returns undefined if email does not exists

--- tests/infra/postgres/repositories/user-account-repository.spec.ts
+++ tests/infra/postgres/repositories/user-account-repository.spec.ts
@@ -3,7 +3,7 @@ import { newDb } from 'pg-mem'
 import { ILoadUserAccountRepository } from '@data/protocols/repositories'
 import { Entity, PrimaryGeneratedColumn, Column, getRepository } from 'typeorm'
 
-class PostgresUserAccountRepository implements ILoadUserAccountRepository {
+export class PostgresUserAccountRepository implements ILoadUserAccountRepository {
   async load (params: ILoadUserAccountRepository.Params): Promise<ILoadUserAccountRepository.Result> {
     const postgresUserRepository = getRepository(PostgresUser)
     const postgresUser = await postgresUserRepository.findOne({ email: params.email })
@@ -33,9 +33,9 @@ class PostgresUser {
 
 describe('user-account repository', () => {
   describe('load', () => {
-    test('', async () => {
-      const db = newDb()
-      const connection = await db.adapters.createTypeormConnection({
+    test('should return an account if email exists', async () => {
+      const database = newDb()
+      const connection = await database.adapters.createTypeormConnection({
         type: 'postgres',
         entities: [PostgresUser]
       })
@@ -46,6 +46,20 @@ describe('user-account repository', () => {
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    test('should return undefined if email does not exists', async () => {
+      const database = newDb()
+      const connection = await database.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PostgresUser]
+      })
+      await connection.synchronize()
+      const sut = new PostgresUserAccountRepository()
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
     })
   })
 })
