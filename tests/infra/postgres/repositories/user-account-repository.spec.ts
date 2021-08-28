c21495b812a43e71e00c6edcf3a064a4a3888d0b
Leandro2585 <leo.real2585@gmail.com>
2021-08-27 23:03:34 -0300

    test: ensure PostgresUserAccountRepository create an account if id is undefined

--- tests/infra/postgres/repositories/user-account-repository.spec.ts
+++ tests/infra/postgres/repositories/user-account-repository.spec.ts
@@ -6,26 +6,26 @@ import { PostgresUserAccountRepository } from '@infra/postgres/repositories'
 import { makeFakeDatabase } from '../mocks/mock-connection'
 
 describe('user-account repository', () => {
+  let backup: IBackup
+  let postgresUserRepository: Repository<PostgresUser>
+  let sut: PostgresUserAccountRepository
+
+  beforeAll(async () => {
+    const database = await makeFakeDatabase([PostgresUser])
+    backup = database.backup()
+    postgresUserRepository = getRepository(PostgresUser)
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new PostgresUserAccountRepository()
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
   describe('load', () => {
-    let backup: IBackup
-    let postgresUserRepository: Repository<PostgresUser>
-    let sut: PostgresUserAccountRepository
-
-    beforeAll(async () => {
-      const database = await makeFakeDatabase([PostgresUser])
-      backup = database.backup()
-      postgresUserRepository = getRepository(PostgresUser)
-    })
-
-    beforeEach(() => {
-      backup.restore()
-      sut = new PostgresUserAccountRepository()
-    })
-
-    afterAll(async () => {
-      await getConnection().close()
-    })
-
     test('should return an account if email exists', async () => {
       await postgresUserRepository.save({ email: 'existing_email' })
       const account = await sut.load({ email: 'existing_email' })
@@ -37,6 +37,19 @@ describe('user-account repository', () => {
       const account = await sut.load({ email: 'new_email' })
 
       expect(account).toBeUndefined()
+    })
+  })
+
+  describe('saveWithFacebook', () => {
+    test('should create an account if id is undefined', async () => {
+      await sut.saveWithFacebook({
+        email: 'any_email',
+        name: 'any_name',
+        facebookId: 'any_fb_id'
+      })
+      const postgresUser = await postgresUserRepository.findOne({ email: 'any_email' })
+
+      expect(postgresUser?.id).toBe(1)
     })
   })
 })
