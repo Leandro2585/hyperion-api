a76a50691481738aad1c992323670fc996c9bbae
Leandro2585 <leo.real2585@gmail.com>
2021-08-31 20:44:21 -0300

    test: ensure RequiredStringValidator returns Error if value is empty

---
+++ tests/app/validation/required-string.spec.ts
@@ -0,0 +1,35 @@
+import { RequiredFieldError } from '@app/errors'
+
+export class RequiredStringValidator {
+  constructor (
+    private readonly value: string,
+    private readonly fieldName: string
+  ) {}
+
+  validate (): Error | undefined {
+    return new RequiredFieldError('any_field')
+  }
+}
+
+describe('required-string validator', () => {
+  test('should return RequiredFieldError if value is empty', () => {
+    const sut = new RequiredStringValidator('', 'any_field')
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  test('should return RequiredFieldError if value is null', () => {
+    const sut = new RequiredStringValidator(null as any, 'any_field')
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+
+  test('should return RequiredFieldError if value is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field')
+    const error = sut.validate()
+
+    expect(error).toEqual(new RequiredFieldError('any_field'))
+  })
+})
