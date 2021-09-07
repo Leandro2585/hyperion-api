8e8276c2a5cb55865528f1a1e2ba966abadaabdd
Leandro2585 <leo.real2585@gmail.com>
2021-09-06 22:25:57 -0300

    feat: add express

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,9 @@
+import './config/module-alias'
 import 'reflect-metadata'
+import express from 'express'
+
+const app = express()
+
+app.listen(3333, () => {
+  console.log('Server started at http://localhost:3333/')
+})
