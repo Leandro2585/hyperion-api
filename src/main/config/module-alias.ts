57dba42ab3ac7af527fa21e2b40bae7e58b30ee3
Leandro2585 <leo.real2585@gmail.com>
2021-09-06 22:47:12 -0300

    test: ensure facebook-api return a Facebook User if token is valid

--- src/main/config/module-alias.ts
+++ src/main/config/module-alias.ts
@@ -1,4 +1,8 @@
 import { addAlias } from 'module-alias'
 import { resolve } from 'path'
 
-addAlias('@', resolve('build'))
+addAlias('@app', resolve('build/app'))
+addAlias('@data', resolve('build/data'))
+addAlias('@main', resolve('build/main'))
+addAlias('@infra', resolve('build/infra'))
+addAlias('@domain', resolve('build/domain'))
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,3 +1,21 @@
-describe('', () => {
-  test('', () => {})
+import { FacebookApi } from '@infra/apis'
+import { AxiosHttpClientAdapter } from '@infra/http'
+import { env } from '@main/config/env'
+
+describe('facebook-api integration tests', () => {
+  test('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClientAdapter()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+    const facebookUser = await sut.loadUser({ token: 'EAAE3ZAgN179UBAP0FMG3kbtFcYLZB5VxqXorJSSbl4gG78MYIpDOnAZAyblPbRatRnPDz3mEEU7WqZCDFxs8ZC7SpuZBjGml9D7AUBcqDmiA9OVgidsKFPY6g3DXEIZBZAaDeVuHDqEMYtHgcX9XM0Y0Q9oR2kVLmSLlxvx83ZCkn1a6tRKn8LrJfccDJZBto7SVgBVZAtwPiB1nzzoLtEsnAcF' })
+
+    expect(facebookUser).toEqual({
+      facebookId: '203799838350394',
+      email: 'leo_uwvkitx_teste@tfbnw.net',
+      name: 'Leo Teste'
+    })
+  })
 })
