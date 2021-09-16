98031cfe449b32a207de8f425549a133701510c1
Leandro2585 <leo.real2585@gmail.com>
2021-09-16 19:26:22 -0300

    test: ensure Authorize returns accessToken

--- tests/core/usecases/authorize-service.spec.ts
+++ tests/core/usecases/authorize-service.spec.ts
@@ -3,17 +3,20 @@ import { mock, MockProxy } from 'jest-mock-extended'
 jest.mock('@core/models/facebook-account')
 
 interface ITokenValidator {
-  validateToken: (params: ITokenValidator.Params) => Promise<void>
+  validateToken: (params: ITokenValidator.Params) => Promise<ITokenValidator.Result>
 }
 
 export namespace ITokenValidator {
   export type Params = { token: string }
+  export type Result = string
 }
 type Setup = (crypto: ITokenValidator) => Authorize
 type Input = { token: string }
-type Authorize = (params: Input) => Promise<void>
+type Output = string
+type Authorize = (params: Input) => Promise<Output>
 const setupAuthorize: Setup = (crypto) => async (params) => {
-  await crypto.validateToken(params)
+  const key = await crypto.validateToken(params)
+  return key
 }
 
 describe('authorize usecase', () => {
@@ -24,6 +27,7 @@ describe('authorize usecase', () => {
   beforeAll(() => {
     token = 'any_token'
     crypto = mock()
+    crypto.validateToken.mockResolvedValue('any_value')
   })
 
   beforeEach(() => {
@@ -35,5 +39,11 @@ describe('authorize usecase', () => {
 
     expect(crypto.validateToken).toHaveBeenCalledWith({ token })
     expect(crypto.validateToken).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return the correct accessToken', async () => {
+    const userId = await sut({ token })
+
+    expect(userId).toBe('any_value')
   })
 })
