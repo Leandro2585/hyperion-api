459647b3236aeddf69339dd3e1dd2db35ee7b1e7
Leandro2585 <leo.real2585@gmail.com>
2021-09-20 21:32:48 -0300

    test: ensure authentication middleware calls authorize with correct params

--- tests/app/middlewares/authentication-middleware.spec.ts
+++ tests/app/middlewares/authentication-middleware.spec.ts
@@ -1,22 +1,35 @@
 import { HttpResponse } from '@app/protocols'
 import { ForbiddenError } from '@app/errors'
 import { forbidden } from '@app/helpers/http-helpers'
+import { RequiredStringValidator } from '@app/validators'
+import { AuthorizeService } from '@core/usecases'
 
 type HttpRequest = {
   authorization: string
 }
 
 export class AuthenticationMiddleware {
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
-    return forbidden()
+  constructor (private readonly authorize: AuthorizeService) {}
+
+  async handle ({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate()
+    if (error !== undefined) return forbidden()
+    await this.authorize({ token: authorization })
   }
 }
 
 describe('authentication middleware', () => {
   let sut: AuthenticationMiddleware
+  let authorization: string
+  let authorize: jest.Mock
+
+  beforeAll(() => {
+    authorization = 'any_authorization_token'
+    authorize = jest.fn()
+  })
 
   beforeEach(() => {
-    sut = new AuthenticationMiddleware()
+    sut = new AuthenticationMiddleware(authorize)
   })
 
   test('should return 403 if authorization is empty', async () => {
@@ -44,5 +57,11 @@ describe('authentication middleware', () => {
       statusCode: 403,
       data: new ForbiddenError()
     })
+  })
+
+  test('should call authorize with correct params', async () => {
+    await sut.handle({ authorization })
+    expect(authorize).toHaveBeenCalledWith({ token: authorization })
+    expect(authorize).toHaveBeenCalledTimes(1)
   })
 })
