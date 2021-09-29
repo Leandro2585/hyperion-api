d78cb6ee04276229d8e46b38ff64cf617170e564
Leandro2585 <leo.real2585@gmail.com>
2021-09-29 20:48:52 -0300

    test: ensure upload-profile-avatar usecase calls UploadFile with unique key

--- tests/core/usecases/upload-profile-avatar-service.spec.ts
+++ tests/core/usecases/upload-profile-avatar-service.spec.ts
@@ -1,11 +1,11 @@
 import { mock } from 'jest-mock-extended'
 
-type Setup = (fileStorage: UploadFile) => UploadProfileAvatar
+type Setup = (fileStorage: UploadFile, cryptography: UUIDGenerator) => UploadProfileAvatar
 type Input = { userId: string, file: Buffer }
 type UploadProfileAvatar = (input: Input) => Promise<void>
 
-const setupUploadProfileAvatar: Setup = (fileStorage) => async ({ file, userId }) => {
-  await fileStorage.upload({ file, key: userId })
+const setupUploadProfileAvatar: Setup = (fileStorage, cryptography) => async ({ file, userId }) => {
+  await fileStorage.upload({ file, key: cryptography.uuid({ key: userId }) })
 }
 
 interface UploadFile {
@@ -16,14 +16,26 @@ export namespace UploadFile {
   export type Params = { file: Buffer, key: string }
 }
 
+interface UUIDGenerator {
+  uuid: (params: UUIDGenerator.Params) => UUIDGenerator.Result
+}
+
+export namespace UUIDGenerator {
+  export type Params = { key: string }
+  export type Result = string
+}
+
 describe('upload-profile-avatar usecase', () => {
-  test('should call IUploadFile with correct params', async () => {
-    const file = Buffer.from('any_budder')
+  test('should call UploadFile with correct params', async () => {
+    const uuid = 'any_unique_id'
+    const file = Buffer.from('any_buffer')
     const fileStorage = mock<UploadFile>()
-    const sut = setupUploadProfileAvatar(fileStorage)
+    const cryptography = mock<UUIDGenerator>()
+    cryptography.uuid.mockReturnValue(uuid)
+    const sut = setupUploadProfileAvatar(fileStorage, cryptography)
     await sut({ userId: 'any_user_id', file })
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_user_id' })
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid })
     expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
 })
