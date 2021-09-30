dd84536216c0a9e5cc4c130c13a32e1636708969
Leandro2585 <leo.real2585@gmail.com>
2021-09-30 20:39:55 -0300

    feat: ensure ChangeProfileAvatar dont call FileStorage when file is undefined

--- src/core/usecases/change-profile-avatar-service.ts
+++ src/core/usecases/change-profile-avatar-service.ts
@@ -1,9 +1,11 @@
 import { UploadFile, UUIDGenerator } from '@core/protocols/gateways'
 
 type Setup = (fileStorage: UploadFile, cryptography: UUIDGenerator) => ChangeProfileAvatarService
-type Input = { userId: string, file: Buffer }
+type Input = { userId: string, file?: Buffer }
 export type ChangeProfileAvatarService = (input: Input) => Promise<void>
 
 export const setupUploadProfileAvatar: Setup = (fileStorage, cryptography) => async ({ file, userId }) => {
-  await fileStorage.upload({ file, key: cryptography.uuid({ key: userId }) })
+  if (file !== undefined) {
+    await fileStorage.upload({ file, key: cryptography.uuid({ key: userId }) })
+  }
 }
