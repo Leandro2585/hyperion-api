6589ae087b0f51381c4578a8ce309df6e5bf55ff
Leandro2585 <leo.real2585@gmail.com>
2021-10-03 09:48:31 -0300

    test: ensure UUIDAdapter returns correct value

--- tests/infra/cryptography/uuid-adapter.spec.ts
+++ tests/infra/cryptography/uuid-adapter.spec.ts
@@ -1,9 +1,10 @@
 import { UUIDGenerator } from '@core/protocols/cryptography'
+import { mocked } from 'ts-jest/utils'
 import { v4 } from 'uuid'
 
 export class UUIDAdapter implements UUIDGenerator {
-  uuid (args: UUIDGenerator.Input): string {
-    return v4()
+  uuid (args: UUIDGenerator.Input): UUIDGenerator.Output {
+    return `${args.key}_${v4()}`
   }
 }
 
@@ -15,5 +16,13 @@ describe('uuid adapter', () => {
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
+  })
+
+  test('should return correct uuid', () => {
+    mocked(v4).mockReturnValueOnce('any_uuid')
+    const sut = new UUIDAdapter()
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_any_uuid')
   })
 })
