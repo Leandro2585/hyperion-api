b44aad3397be94a73a3e38c44884fc08fa4394e2
Leandro2585 <leo.real2585@gmail.com>
2021-10-04 19:33:27 -0300

    refactor: move UniqueId to its own file

--- src/infra/cryptography/index.ts
+++ src/infra/cryptography/index.ts
@@ -1,1 +1,3 @@
 export * from './jwt-token-adapter'
+export * from './unique-id-adapter'
+export * from './uuid-adapter'
---
+++ src/infra/cryptography/unique-id-adapter.ts
@@ -0,0 +1,15 @@
+import { UUIDGenerator } from '@core/protocols/cryptography'
+
+export class UniqueId implements UUIDGenerator {
+  constructor (private readonly date: Date) {}
+
+  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
+    return key + '_' +
+    this.date.getFullYear() +
+    (this.date.getMonth() + 1).toString().padStart(2, '0') +
+    this.date.getDate().toString().padStart(2, '0') +
+    this.date.getHours().toString().padStart(2, '0') +
+    this.date.getMinutes().toString().padStart(2, '0') +
+    this.date.getSeconds().toString().padStart(2, '0')
+  }
+}
