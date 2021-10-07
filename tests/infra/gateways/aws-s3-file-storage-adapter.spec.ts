58afaf766063bf7ca6414d7b1e88f40cc7244d14
Leandro2585 <leo.real2585@gmail.com>
2021-10-06 21:47:18 -0300

    test: ensure AwsFileStorage calls putObject with correct args

--- tests/infra/gateways/aws-s3-file-storage-adapter.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage-adapter.spec.ts
@@ -1,13 +1,26 @@
-import { config } from 'aws-sdk'
+import { config, S3 } from 'aws-sdk'
+import { mocked } from 'ts-jest/utils'
 
-class AwsS3FileStorageAdapter {
-  constructor (private readonly accessKey: string, private readonly secret: string) {
+import { UploadFile } from '@core/protocols/gateways'
+
+export class AwsS3FileStorageAdapter {
+  constructor (accessKey: string, secret: string, private readonly bucket: string) {
     config.update({
       credentials: {
         accessKeyId: accessKey,
         secretAccessKey: secret
       }
     })
+  }
+
+  async upload ({ key, file }: UploadFile.Input): Promise<void> {
+    const s3 = new S3()
+    await s3.putObject({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    }).promise()
   }
 }
 
@@ -16,15 +29,28 @@ jest.mock('aws-sdk')
 describe('aws-s3-file-storage adapter', () => {
   let accessKey: string
   let secret: string
+  let bucket: string
+  let key: string
+  let file: Buffer
   let sut: AwsS3FileStorageAdapter
-
-  beforeEach(() => {
-    sut = new AwsS3FileStorageAdapter(accessKey, secret)
-  })
+  let putObjectPromiseSpy: jest.Mock
+  let putObjectSpy: jest.Mock
 
   beforeAll(() => {
     accessKey = 'any_access_key'
     secret = 'any_secret'
+    bucket = 'any_bucket'
+    key = 'any_key'
+    file = Buffer.from('any_buffer')
+    putObjectPromiseSpy = jest.fn()
+    putObjectSpy = jest.fn().mockImplementationOnce(() => ({ promise: putObjectPromiseSpy }))
+    mocked(S3).mockImplementationOnce(jest.fn().mockImplementationOnce(() => ({
+      putObject: putObjectSpy
+    })))
+  })
+
+  beforeEach(() => {
+    sut = new AwsS3FileStorageAdapter(accessKey, secret, bucket)
   })
 
   test('should config aws credentials on creation', () => {
@@ -34,5 +60,18 @@ describe('aws-s3-file-storage adapter', () => {
       secretAccessKey: secret
     })
     expect(config.update).toHaveBeenCalledTimes(1)
+  })
+
+  test('should call putObject with correct args', async () => {
+    await sut.upload({ key, file })
+
+    expect(putObjectSpy).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    })
+    expect(putObjectSpy).toHaveBeenCalledTimes(1)
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
   })
 })
