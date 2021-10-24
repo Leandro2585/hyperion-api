3cb17c1cd4539baa58a6adcf6c247c3a22024f08
Leandro2585 <leo.real2585@gmail.com>
2021-10-24 10:05:37 -0300

    test: ensure allowed-mime-types validator returns undefined if value is valid

--- tests/app/validators/allowed-mime-types.spec.ts
+++ tests/app/validators/allowed-mime-types.spec.ts
@@ -8,8 +8,10 @@ export class AllowedMimeTypesValidator {
     private readonly mimeType: string
   ) {}
 
-  validate(): Error {
-    return new InvalidMimeTypeError(this.allowedExtensions)
+  validate(): Error | undefined {
+    if(this.allowedExtensions.includes('png') && this.mimeType !== 'image/png') {
+      return new InvalidMimeTypeError(this.allowedExtensions)
+    }
   }
 }
 
@@ -19,5 +21,12 @@ describe('allowed-mime-types validator', () => {
     const error = sut.validate()
 
     expect(error).toEqual(new InvalidMimeTypeError(['png']))
+  })
+
+  test('should return undefined if value is valid', () => {
+    const sut = new AllowedMimeTypesValidator(['png'], 'image/png')
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
   })
 })
