82e0f43a3ec2aa507e12de1319858ffdbfa77af4
Leandro2585 <leo.real2585@gmail.com>
2021-10-25 20:09:27 -0300

    test: ensure validation-builder validator returns RequiredBufferValidator

--- src/app/validators/validation-builder.ts
+++ src/app/validators/validation-builder.ts
@@ -1,15 +1,15 @@
 import { Validator } from '@app/protocols'
-import { RequiredStringValidator } from '@app/validators'
+import { RequiredStringValidator, RequiredValidator, RequiredBufferValidator } from '@app/validators'
 
 type BuilderParams = {
-  value: string
-  fieldName: string
+  value: any
+  fieldName?: string
 }
 
 export class ValidationBuilder {
   private constructor (
-    private readonly value: string,
-    private readonly fieldName: string,
+    private readonly value: any,
+    private readonly fieldName?: string,
     private readonly validators: Validator[] = []
   ) {}
 
@@ -17,7 +17,24 @@ export class ValidationBuilder {
     return new ValidationBuilder(params.value, params.fieldName)
   }
 
-  required (): ValidationBuilder {
+  required(): ValidationBuilder {
+    if(this.value instanceof Buffer) return this.buffer()
+    else if (typeof this.value === 'string') return this.string()
+    else if (typeof this.value === 'object') return this.object()
+    return this
+  }
+
+  object(): ValidationBuilder {
+    this.validators.push(new RequiredValidator(this.value, this.fieldName))
+    return this
+  }
+
+  buffer (): ValidationBuilder {
+    this.validators.push(new RequiredBufferValidator(this.value, this.fieldName))
+    return this
+  }
+
+  string (): ValidationBuilder {
     this.validators.push(new RequiredStringValidator(this.value, this.fieldName))
     return this
   }
--- tests/app/validators/validation-builder.spec.ts
+++ tests/app/validators/validation-builder.spec.ts
@@ -1,12 +1,19 @@
-import { RequiredStringValidator, ValidationBuilder } from '@app/validators'
+import { RequiredBufferValidator, RequiredStringValidator, ValidationBuilder } from '@app/validators'
 
 describe('validation-builder', () => {
-  test('should return a RequiredStringValidator', () => {
+  test('should return RequiredStringValidator', () => {
     const validators = ValidationBuilder
-      .of({ value: 'any_value', fieldName: 'any_name' })
+      .of({ value: 'any_value' })
       .required()
       .build()
 
     expect(validators).toEqual([new RequiredStringValidator('any_value', 'any_name')])
+  })
+
+  test('should return RequiredBufferValidator', () => {
+    const buffer = Buffer.from('any_buffer')
+    const validators = ValidationBuilder.of({ value: buffer }).required().build()
+  
+    expect(validators).toEqual([new RequiredBufferValidator(buffer)])
   })
 })
