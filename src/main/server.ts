299b1ad7f79191620fe48027c693181b2d083dcc
Leandro2585 <leo.real2585@gmail.com>
2021-11-02 10:37:01 -0300

    refactor: move typeorm connection options from server.ts to ormconfig.js

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,11 +4,8 @@ import { createConnection, getConnectionOptions } from 'typeorm'
 import './config/module-alias'
 import { app, env } from '@main/config'
 
-getConnectionOptions()
-  .then(async (options) => {
-    const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
-    const entities = [`${root}/infra/typeorm/entities/entities/index.{js,ts}`]
-    await createConnection({ ...options, entities })
+createConnection()
+  .then(async () => {
     const { app } = await import('@main/config')
     app.listen(env.apiPort, () => {
       console.log(`Server started at http://localhost:${env.apiPort}/`)
