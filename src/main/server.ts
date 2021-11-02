7670927c4d4f5d3d74b4d9c0f8154e251f52af9f
Leandro2585 <leo.real2585@gmail.com>
2021-11-02 09:16:20 -0300

    fix: resolve connection problems

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,17 @@
 import 'reflect-metadata'
-import { createConnection } from 'typeorm'
+import { createConnection, getConnectionOptions } from 'typeorm'
 
-import '@main/config/module-alias'
+import './config/module-alias'
 import { app, env } from '@main/config'
 
-createConnection('default')
-  .then(() => {
+getConnectionOptions()
+  .then(async (options) => {
+    const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
+    const entities = [`${root}/infra/typeorm/entities/entities/index.{js,ts}`]
+    await createConnection({ ...options, entities })
+    const { app } = await import('@main/config')
     app.listen(env.apiPort, () => {
       console.log(`Server started at http://localhost:${env.apiPort}/`)
     })
-  }).catch(console.error)
+  })
+  .catch(console.error)
--- tests/infra/typeorm/mocks/mock-connection.ts
+++ tests/infra/typeorm/mocks/mock-connection.ts
@@ -4,7 +4,7 @@ export const makeFakeDatabase = async (entities?: any[]): Promise<IMemoryDb> => 
   const database = newDb()
   const connection = await database.adapters.createTypeormConnection({
     type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts']
+    entities: entities ?? ['src/infra/typeorm/entities/index.ts']
   })
   await connection.synchronize()
   return database
