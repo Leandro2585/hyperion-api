c4f6d2f43a9d7caea7b885df8400dba993b399ec
Leandro2585 <leo.real2585@gmail.com>
2021-11-06 09:49:07 -0300

    test: ensure DatabaseTransactionDecorator executes decoratee

--- tests/app/decorators/database-transaction-decorator.spec.ts
+++ tests/app/decorators/database-transaction-decorator.spec.ts
@@ -1,9 +1,16 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
+import { Controller } from '@app/protocols'
+
 export class DatabaseTransactionDecorator {
-  constructor (private readonly database: DatabaseTransaction) {}
+  constructor (
+    private readonly decoratee: Controller, 
+    private readonly database: DatabaseTransaction
+  ) {}
+  
   async execute(httpRequest: any): Promise<void> {
     await this.database.openTransaction()
+    await this.decoratee.execute(httpRequest)
   }
 }
 
@@ -13,21 +20,29 @@ export interface DatabaseTransaction {
 
 describe('database-transaction decorator', () => {
   let database: MockProxy<DatabaseTransaction>
+  let decoratee: MockProxy<Controller>
   let sut: DatabaseTransactionDecorator
 
   beforeAll(() => {
     database = mock()
+    decoratee = mock()
   })
   
   beforeEach(() => {
-    sut = new DatabaseTransactionDecorator(database)
+    sut = new DatabaseTransactionDecorator(decoratee, database)
   })
 
   test('should open transaction', async () => {
-    
     await sut.execute({ any: 'any' })
 
     expect(database.openTransaction).toHaveBeenCalledWith()
     expect(database.openTransaction).toHaveBeenCalledTimes(1)
+  })
+
+  test('should execute decoratee', async () => {
+    await sut.execute({ any: 'any' })
+
+    expect(decoratee.execute).toHaveBeenCalledWith({ any: 'any' })
+    expect(decoratee.execute).toHaveBeenCalledTimes(1)
   })
 })
