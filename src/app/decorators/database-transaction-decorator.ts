45239935f6cdd1af2f814c975f1a78978ac2381d
Leandro2585 <leo.real2585@gmail.com>
2021-11-06 10:42:42 -0300

    feat: add database-transaction decorator

---
+++ src/app/decorators/database-transaction-decorator.ts
@@ -0,0 +1,22 @@
+import { Controller, DatabaseTransaction, HttpResponse } from '@app/protocols'
+
+export class DatabaseTransactionDecorator extends Controller {
+  constructor (
+    private readonly decoratee: Controller, 
+    private readonly database: DatabaseTransaction
+  ) { super() }
+  
+  async execute(httpRequest: any): Promise<HttpResponse> {
+    await this.database.openTransaction()
+    try {
+      const httpResponse = await this.decoratee.execute(httpRequest)
+      await this.database.commit()
+      return httpResponse
+    } catch (error) {
+      await this.database.rollback()
+      throw error
+    } finally {
+      await this.database.closeTransaction()
+    }
+  }
+}
--- tests/app/decorators/database-transaction-decorator.spec.ts
+++ tests/app/decorators/database-transaction-decorator.spec.ts
@@ -1,34 +1,7 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
-import { Controller, HttpResponse } from '@app/protocols'
-
-export class DatabaseTransactionDecorator extends Controller {
-  constructor (
-    private readonly decoratee: Controller, 
-    private readonly database: DatabaseTransaction
-  ) { super() }
-  
-  async execute(httpRequest: any): Promise<HttpResponse> {
-    await this.database.openTransaction()
-    try {
-      const httpResponse = await this.decoratee.execute(httpRequest)
-      await this.database.commit()
-      return httpResponse
-    } catch (error) {
-      await this.database.rollback()
-      throw error
-    } finally {
-      await this.database.closeTransaction()
-    }
-  }
-}
-
-export interface DatabaseTransaction {
-  commit: () => Promise<void>
-  rollback: () => Promise<void>
-  openTransaction: () => Promise<void>
-  closeTransaction: () => Promise<void>
-}
+import { Controller, DatabaseTransaction } from '@app/protocols'
+import { DatabaseTransactionDecorator } from '@app/decorators'
 
 describe('database-transaction decorator', () => {
   let database: MockProxy<DatabaseTransaction>
